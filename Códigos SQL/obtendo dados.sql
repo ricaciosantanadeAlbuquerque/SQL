aa56731953d70f35b624b0f7c3d9f09a9dbc7e2f
ricacio <ricaciozz@gmail.com>
2022-08-01 02:22:24 -0300

    01-08-2022

---
+++ Códigos SQL/obtendo dados.sql
@@ -0,0 +1,33 @@
+use visoes;
+## exe01
+select * from cliente;
+select nome from cliente;
+## exe02;
+select id,nome,cidade,nivel from cliente;
+## exe03;
+select * from cliente;
+## exe04;
+select id from cliente;
+## exe05;
+select distinct id from cliente;
+## exe 06
+select distinctnome from cliente;
+##exe 07
+select distinct cidade from cliente;
+##exe08
+select distinct nome from vendedor;
+##exe09
+select nome from cliente limit 2;
+## exe 10
+select nome from cliente limit 3;
+
+## exe 10
+select id from vendedor;
+## exe 011
+select * from vendedor;
+select  distinct cidade, id from vendedor;
+
+# exe12
+/*select * from vendedor;*/
+
+
